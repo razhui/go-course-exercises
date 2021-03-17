d89f809b0ef02a2623f61be0c877ba9536c32733
razhui <56419009+razhui@users.noreply.github.com>
2021-03-16 18:53:10 -0700

    second exercise

---
+++ exercise-two.go
@@ -0,0 +1,17 @@
+//Using the following operators, write expressions and assign their values to variables
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	g := 10 == 10
+	h := 10 <= 10
+	i := 10 >= 10
+	j := 10 != 10
+	k := 10 < 10
+	l := 10 > 10
+	fmt.Println(g, h, i, j, k, l)
+}
